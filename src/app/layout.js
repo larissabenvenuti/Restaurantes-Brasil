46b0d576bfcdcbcd55722f956d02429def2eddef
Larissa <larissabenvenutia@gmail.com>
2025-03-13 13:39:53 -0300

    Footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,9 +18,20 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-br">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+    <html lang="pt-br" className="m-0 p-0">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased m-0 p-0`}>
         {children}
+        <footer className=" text-black py-6 mt-10">
+          <div className="container mx-auto text-center">
+            <p className="text-sm">
+              Desenvolvido por{" "}
+              <span className="font-semibold">Larissa Benvenuti</span>
+            </p>
+            <p className="text-xs mt-2 text-gray-400">
+              © {new Date().getFullYear()} Todos os direitos reservados.
+            </p>
+          </div>
+        </footer>
       </body>
     </html>
   );
