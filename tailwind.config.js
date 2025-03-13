91c612080fa591a3a709552a46edb7a8e4ce6c1a
Larissa <larissabenvenutia@gmail.com>
2025-03-13 13:51:35 -0300

    Mudança

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 export default {
-    content: [
-      "./app/**/*.{js,ts,jsx,tsx}",
-      "./pages/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
-  
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  experimental: {
+    optimizeUniversalDefaults: false,
+  },
+};
